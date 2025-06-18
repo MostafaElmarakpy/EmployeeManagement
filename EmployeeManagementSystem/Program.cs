cc778ba6e0af621af578e7e1d97faccee7677ded
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-06-18 14:30:24 +0300

    Add new projects and update Program.cs for .NET 9.0
    - Introduced Domain, Application, and Infrastructure projects to the Employee Management solution, targeting .NET 9.0.
    - Updated `Program.cs` to configure ASP.NET Core services, including authorization and authentication.
    - Modified project files to include necessary package references for JSON handling and Entity Framework Core.

--- EmployeeManagementSystem/Program.cs
+++ EmployeeManagementSystem/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+//    options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<AppContext>()
+//    .AddDefaultTokenProviders();
+
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
@@ -9,14 +26,18 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+//app.UseSerilogRequestLogging();
+app.UseStaticFiles();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+app.MapRazorPages();
 
 app.MapStaticAssets();
 
