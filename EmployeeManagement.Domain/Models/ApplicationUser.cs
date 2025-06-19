f75b4bf88e1e9e387159625324877b09fa0dba9d
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-06-19 22:40:50 +0300

    Refactor ApplicationUser properties
    Removed the `FirstName` property and added a new `LastName` property to the `ApplicationUser` class. Both properties are now nullable strings, allowing for optional values.

--- EmployeeManagement.Domain/Models/ApplicationUser.cs
+++ EmployeeManagement.Domain/Models/ApplicationUser.cs
@@ -9,7 +9,8 @@ namespace EmployeeManagement.Domain.Models
 {
     public class ApplicationUser : IdentityUser
     {
-
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedAt { get; set; }
         public bool IsActive { get; set; } = true;
