eb418382c4e467f5df7c42a8cfbf9d607828151e
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-06-22 10:57:02 +0300

    Enhance GetAllAsync to support eager loading
    Added an optional `includeProperties` parameter to the
    `GetAllAsync` method in `GenericRepository.cs`. This
    allows callers to specify related entities to include in
    the query, enabling more flexible data retrieval through
    eager loading while preserving existing filtering capabilities.

--- EmployeeManagement.Infrastructure/Repositories/GenericRepository.cs
+++ EmployeeManagement.Infrastructure/Repositories/GenericRepository.cs
@@ -35,10 +35,20 @@ namespace EmployeeManagement.Infrastructure.Repositories
         public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter = null!, string includeProperties = null!)
         {
             IQueryable<T> query = _dbSet;
+
             if (includeProperties != null)
+            {
                 foreach (var includeProperty in includeProperties.Split(new char[] { ',' },
                     StringSplitOptions.RemoveEmptyEntries))
+                {
                     query = query.Include(includeProperty).AsSplitQuery().AsNoTracking();
+                }
+            }
+
+
+
+
+
 
             if (filter != null)
                 query = query.Where(filter);
