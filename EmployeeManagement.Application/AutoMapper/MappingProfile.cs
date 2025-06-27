c89239bc8f33bd0ae8992df4976272c2f4471686
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-06-27 19:28:23 +0300

    Add mappings for TaskMB and TaskItem in AutoMapper

--- EmployeeManagement.Application/AutoMapper/MappingProfile.cs
+++ EmployeeManagement.Application/AutoMapper/MappingProfile.cs
@@ -16,33 +16,53 @@ namespace EmployeeManagement.Application.AutoMapper
             // Domain to ViewModel  
             CreateMap<Department, DepartmentViewModel>();
 
-
-            // Map Employee to EmployeeViewModel with Department and Manager names
+            // Map Employee to EmployeeViewModel with Department and Manager names and with ImageFile handling  
             CreateMap<Employee, EmployeeViewModel>()
                 .ForMember(dest => dest.DepartmentName, opt => opt.MapFrom(src => src.Department.Name))
                 .ForMember(dest => dest.ManagerName, opt => opt.MapFrom(src => src.Manager != null ? src.Manager.FullName : null))
-                .AfterMap((src, dest) =>
-                {
-                    Console.WriteLine($"Mapping Employee: {src.Id} -> {dest.Id}, Department: {dest.DepartmentName}, Manager: {dest.ManagerName}");
-                });
-
+                .ForMember(dest => dest.ImagePath, opt => opt.MapFrom(src => src.ImagePath));
 
             // ViewModel to Domain  
             CreateMap<DepartmentViewModel, Department>();
-            CreateMap<EmployeeViewModel, Employee>();
+
+            CreateMap<EmployeeViewModel, Employee>()
+                .ForMember(dest => dest.ImagePath, opt => opt.Ignore())
+                .ForMember(dest => dest.ImageFile, opt => opt.Ignore())
+                .ForMember(dest => dest.User, opt => opt.Ignore())
+                .ForMember(dest => dest.ManagedEmployees, opt => opt.Ignore())
+                .ForMember(dest => dest.EmployeeTasks, opt => opt.Ignore());
+
             CreateMap<EmployeeViewModel, EmployeeViewModel>();
 
-            // Custom mapping for EmployeeViewModel to Employee with ImageFile handling  
-            CreateMap<Employee, EmployeeViewModel>()
-                .ForMember(dest => dest.ImagePath, opt => opt.MapFrom(src => src.ImagePath))
-                .AfterMap((src, dest) =>
-                {
-                    Console.WriteLine($"Mapping ImagePath: {src.ImagePath} -> {dest.ImagePath}");
-                });
-            // 
-            CreateMap<TaskMB, TaskItem>();
-            CreateMap<TaskItem, TaskMB>();
+            // Task Mappings 
+            CreateMap<TaskMB, TaskItem>()
+                .ForMember(dest => dest.AssignedEmployee, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedByManager, opt => opt.Ignore())
+                .ForMember(dest => dest.EmployeeTasks, opt => opt.Ignore());
+
+            // Map TaskItem to TaskMB
+
+            CreateMap<TaskItem, TaskMB>()
+                .ForMember(dest => dest.EmployeeName, opt => opt.MapFrom(src => src.AssignedEmployee != null ? src.AssignedEmployee.FullName : null))
+                .ForMember(dest => dest.CreatedByManagerName, opt => opt.MapFrom(src => src.CreatedByManager != null ? src.CreatedByManager.FullName : null))
+                .ForMember(dest => dest.AssignedDate, opt => opt.Ignore()); 
+           
+            
+
+            CreateMap<EmployeeTask, TaskMB>()
+                // Map core task properties from TaskItem
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Task.Id))
+                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Task.Title))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Task.Description))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Task.Status))
+
+                // Map employee information
+                .ForMember(dest => dest.EmployeeName, opt => opt.MapFrom(src => src.Employee.FullName))
+                .ForMember(dest => dest.EmployeeId, opt => opt.MapFrom(src => src.Employee.Id))
+
+                // Map manager information correctly
+                .ForMember(dest => dest.CreatedByManagerName,
+                    opt => opt.MapFrom(src => src.Task.CreatedByManager.FullName));
         }
     }
-
 }
